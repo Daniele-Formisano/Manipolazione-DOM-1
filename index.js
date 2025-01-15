c50f2d087bfa7550896f41dc11f4e88f024c84d9
Daniele <danieleformisano5@gmail.com>
2025-01-15 12:58:57 +0100

    Modificato codice

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const addProduct = () => {
   const productList = document.querySelector("ul");
 
   const $listItem = document.createElement("li");
+  $listItem.style.listStyle = "none";
 
   const $newItem = document.createElement("input");
   $newItem.id = "checkbox";
